39eda185b8f6e9b8bb5292b6974f25ce0ecfd503
Matt Newbold <matt.newbold208@gmail.com>
2021-02-20 11:59:57 -0600

    pt 4 done

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
      @GetMapping
-    public String displayaAddUserForm(){
+    public String displayAddUserForm(){
          return "user/add";
+     }
+     @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verifyPassword){
+    //User.add(user)
+         if(!verifyPassword.equals(user.getPassword())){
+             model.addAttribute("error", "Passwords don't match.");
+             return "user/add";
+         } else {
+             model.addAttribute("user", user);
+             return "user/index";
+         }
      }
 }
